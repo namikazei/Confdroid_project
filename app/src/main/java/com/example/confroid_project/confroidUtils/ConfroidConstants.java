232a1bc7788082d24ee2ba346129d70750bb8b8e
Nabil Bouhar <nbouhar@sephora.fr>
2021-02-07 23:47:51 +0100

    project structurationn and add some utils conversion methods

--- app/src/main/java/com/example/confroid_project/confroidUtils/ConfroidConstants.java
+++ app/src/main/java/com/example/confroid_project/confroidUtils/ConfroidConstants.java
@@ -1,4 +1,4 @@
-package com.example.confroid_project.utils;
+package com.example.confroid_project.confroidUtils;
 
 public class ConfroidConstants {
     public static final String PRIMITIVE_VALUE = "primitive_value";
