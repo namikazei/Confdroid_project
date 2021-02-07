99f06bb071509b9b54bcb69ad20280c8da1859ee
Nabil Bouhar <nbouhar@sephora.fr>
2021-02-07 19:31:32 +0100

    Added Utils Package

---
+++ app/src/main/java/com/example/confroid_project/utils/Utils.java
@@ -0,0 +1,9 @@
+package com.example.confroid_project.utils;
+
+import android.os.Bundle;
+
+public class Utils {
+    public Bundle convertToBundle(Object configuration) {
+        return new Bundle();
+    }
+}
