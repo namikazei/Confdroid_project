24c68b97798adea913a4aef823ef8a1f7788399c
Nabil Bouhar <nbouhar@sephora.fr>
2021-02-28 18:30:09 +0100

    Code adaption (Test app changed !)

--- app/src/main/java/com/example/confroid_project/confroidUtils/ConfroidConstants.java
+++ app/src/main/java/com/example/confroid_project/confroidUtils/ConfroidConstants.java
@@ -7,5 +7,6 @@ public class ConfroidConstants {
     public static final String FIRST_INTENT_REQUEST_TOKEN = "first_intent_request_token";
     public static final String INTENT_TYPE = "intent_type";
     public static final String APPLICATION_NAME = "app_name";
+    public static final String APPLICATION_PACKAGE_NAME = "app_package_name";
     public static final String CONFIGURATION_INTENT = "configuration_intent";
 }
