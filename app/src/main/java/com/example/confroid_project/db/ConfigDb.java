fdf46b5faaf455719aee06a8c1ffcf1a0e739dae
namikazei <massiloman@gmail.com>
2021-03-27 00:04:53 +0100

    implementation 'com.google.code.gson:gson:2.8.6' on build.gradle and somme updates

--- app/src/main/java/com/example/confroid_project/db/ConfigDb.java
+++ app/src/main/java/com/example/confroid_project/db/ConfigDb.java
@@ -141,6 +141,7 @@ public class ConfigDb extends SQLiteOpenHelper {
     public void updateConf(int id, String value){
         ContentValues values = new ContentValues();
         values.put(CONF_CONTENT, value);
+        values.put(CONF_DATE, getDateTime());
 
         SQLiteDatabase db = this.getWritableDatabase();
         db.update(CONFIG_TABLE, values, CONF_ID+"=?", new String[]{String.valueOf(id)});
