b83f8c509e7164732a1cc185d50d774381315743
namikazei <massiloman@gmail.com>
2021-03-27 19:24:42 +0100

    Broad cast receiver to send the token to client app

--- app/src/main/java/com/example/confroid_project/confroidUtils/Utils.java
+++ app/src/main/java/com/example/confroid_project/confroidUtils/Utils.java
@@ -50,20 +50,6 @@ public class Utils {
         return bundle;
     }
 
-    public static String getToken(String appName) {
-        String upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        String lowerAlphabet = "abcdefghijklmnopqrstuvwxyz";
-        String numbers = "0123456789";
-        String alphaNumeric = upperAlphabet + lowerAlphabet + numbers;
-        StringBuilder sb = new StringBuilder();
-        Random random = new Random();
-        int length = 10;
-        for (int i = 0; i < length; i++) {
-            int index = random.nextInt(alphaNumeric.length());
-            char randomChar = alphaNumeric.charAt(index);
-            sb.append(randomChar);
-        }
-        return appName + "_" + sb.toString();
-    }
+
 
 }
--- app/src/main/java/com/example/confroid_project/receivers/TokenDispenser.java
+++ app/src/main/java/com/example/confroid_project/receivers/TokenDispenser.java
@@ -4,19 +4,33 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import java.util.Random;
+
 public class TokenDispenser extends BroadcastReceiver {
+    public static String getToken(String appName) {
+        String upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String lowerAlphabet = "abcdefghijklmnopqrstuvwxyz";
+        String numbers = "0123456789";
+        String alphaNumeric = upperAlphabet + lowerAlphabet + numbers;
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+        int length = 10;
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(alphaNumeric.length());
+            char randomChar = alphaNumeric.charAt(index);
+            sb.append(randomChar);
+        }
+        return appName + "_" + sb.toString();
+    }
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        // AF : refactor AppsIntentsManagerActivity en TokenDispenser ?
-        /*
-        if (intent != null && intent.getAction() != null && intent.getAction().equals("com.example.confroid_project.receivers.tokendispenser.login")) {
-            Bundle bundle = intent.getBundleExtra("data");
-            if (bundle != null) {
-                String appname = bundle.getString("appname");
-                Log.d("Received", appname);
-                sendToken(appname);
-            }
-        }
-         */
+        String receiver = intent.getStringExtra("receiver");
+        String name = intent.getStringExtra("name");
+        //calculat token and send the intent
+        Intent response = new Intent();
+        response.setClassName(name, receiver);
+        response.putExtra("token", getToken(name));
+        context.startService(response);
     }
 }
