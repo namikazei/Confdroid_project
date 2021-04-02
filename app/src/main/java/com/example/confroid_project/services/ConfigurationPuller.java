cd08e46f27c985a082a84fecb3a33009fe78055e
namikazei <massiloman@gmail.com>
2021-04-02 23:31:20 +0200

    update puller

--- app/src/main/java/com/example/confroid_project/services/ConfigurationPuller.java
+++ app/src/main/java/com/example/confroid_project/services/ConfigurationPuller.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import androidx.annotation.Nullable;
 
 import com.example.confroid_project.db.ConfigDb;
+import com.example.confroid_project.db.ConfigurationVersions;
 
 public class ConfigurationPuller extends Service {
 
@@ -17,25 +18,32 @@ public class ConfigurationPuller extends Service {
 
         ConfigDb db = new ConfigDb(this.getApplicationContext());
 
-        String config;
+        ConfigurationVersions config;
 
         String app_token = intent.getStringExtra("token");
         String app_name = intent.getStringExtra("app_name");
 
-        if (db.getAppToken(app_name).equals(app_token)){
+        if (db.getAppToken(app_name).equals(app_token)) {
             int version = intent.getIntExtra("version", -1);
             int requestId = intent.getIntExtra("requestId", 0);
             String receiver = intent.getStringExtra("receiver");
             String conf_name = intent.getStringExtra("name");
 
-            if (version==-1){
-                config = db.getLastConfiguration(app_name, conf_name).getValue();
-            }else{
-                config = db.getConfiguration(app_name, conf_name, version).getValue();
+            if (version == -1) {
+                config =  db.getLastConfiguration(app_name, conf_name);
+            } else {
+                config =  db.getConfiguration(app_name, conf_name, version);
             }
 
             Intent outgoingIntent = new Intent("PULL_CONF_SERVICE");
-            outgoingIntent.putExtra("content", config);
+            if (config.getValue() == null) {
+                Log.e("conf value", "does not exist");
+            } else {
+                outgoingIntent.putExtra("content", config.getValue());
+                outgoingIntent.putExtra("config_name", config.getName());
+            }
+
+
             outgoingIntent.putExtra("requestId", requestId);
             outgoingIntent.setClassName(app_name, receiver);
             ComponentName c = this.startService(outgoingIntent);
@@ -43,10 +51,10 @@ public class ConfigurationPuller extends Service {
             if (c == null)
                 Log.e("faillllll", "failed to start with " + outgoingIntent);
             else
-                Log.d("senddd", config);
+                Log.d("senddd", config.getValue()==null?"NULL":config.getValue());
         }
 
-        return START_NOT_STICKY;
+        return START_STICKY;
     }
 
     @Nullable
